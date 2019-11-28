3fd5a9c9e7553c44baf02dc1ddf911a7ecc1da1c
k1901172 <k1901172@C-A3503-01>
2019-11-28 19:27:27 +0200

    Add project files.

---
+++ TietokantaTesti/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TietokantaTesti
+{
+    class Book
+    {
+        public int Bookid { get; set; }
+        public string Title { get; set; }
+        public int Authorid { get; set; }
+        public int Publisherid { get; set; }
+        public int Pages { get; set; }
+        public double Price { get; set; }
+        public DateTime Date { get; set; }
+
+        public Book()
+        {
+
+        }
+
+        public Book(int id, string title, int authorid, int publisherid, int pages, double price, DateTime date)
+        {
+            this.Bookid = id;
+            this.Title = title;
+            this.Authorid = authorid;
+            this.Publisherid = publisherid;
+            this.Pages = pages;
+            this.Price = price;
+            this.Date = date;
+        }
+
+        public override string ToString()
+        {
+            return this.Bookid + " " + this.Title + " " + this.Price;
+        }
+    }
+}
---
+++ TietokantaTesti/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace TietokantaTesti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify connection options and open an connection
+            NpgsqlConnection conn =
+                new NpgsqlConnection(
+                    "Server=127.0.0.1;User Id=postgres;" +
+                    "Password=Grespost99;Database=books;");
+            conn.Open();
+
+            // Define a query
+            NpgsqlCommand cmd = new NpgsqlCommand("select * from book", conn);
+
+            // Execute a query
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+
+            // Read all rows and output the first column in each row
+            while (dr.Read())
+            {
+                string id =dr[0].ToString();
+                string title = dr[1].ToString();
+                Console.WriteLine(id + " " + title);
+                // Book b = new Book(int.Parse(dr[0].ToString()), dr[1], dr[2], dr[3], dr[4], dr[5], dr[6]);
+            }
+                //Console.Write("{0} {1}\n", dr[0], dr[1]);
+
+            // Close connection
+            conn.Close();
+        }
+    }
+}
